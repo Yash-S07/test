26e93a15de23e825fa7fa0e41ba8c28191b0ccad
Yash_Singhal <yosinghal07@gmail.com>
2024-06-16 01:17:11 +0530

    Delete skyline directory

--- skyline/skyline/urls.py
+++
@@ -1,9 +0,0 @@
-# skyline/urls.py
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('contact.urls')),  # This will include all the paths defined in contact/urls.py
-]
